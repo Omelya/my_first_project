153fec9f112b0297819d822ae5a2bac8b01a2704
Omelyko <sekret.krut@gmail.com>
2021-07-05 00:01:33 +0300

    Edited SearchesController

--- app/Http/Controllers/SearchesController.php
+++ app/Http/Controllers/SearchesController.php
@@ -39,24 +39,87 @@ class SearchesController extends Controller
     public function result(Search $search, Request $request)
     {
       $name = $request->only(['name']);
-      if(!empty($name['name'])){
-        foreach ($name as $names => $name) {
-          $name;
-        }
+        if(!empty($name['name'])){
+          foreach ($name as $names => $name) {
+            $name;
+          }
       } else $name = 0;
+
       $priceFrom = $request->only(['priceFrom']);
+      if(!empty($priceFrom['priceFrom'])){
+        foreach ($priceFrom as $priceFroms => $priceFrom) {
+          $priceFrom;
+        }
+      } else $priceFrom = false;
+
       $priceTo = $request->only(['priceTo']);
+      if(!empty($priceTo['priceTo'])){
+        foreach ($priceTo as $pricesTo => $priceTo) {
+          $priceTo;
+        }
+      } else $priceTo = false;
+
       $bedrooms = $request->only(['bedrooms']);
+        if(!empty($bedrooms['bedrooms'])){
+          foreach ($bedrooms as $bedroom => $bedrooms) {
+            $bedrooms;
+          }
+        } else $bedrooms = false;
+
       $bathrooms = $request->only(['bathrooms']);
+        if(!empty($bathrooms['bathrooms'])){
+          foreach ($bathrooms as $bathroom => $bathrooms) {
+            $bathrooms;
+          }
+        } else $bathrooms = false;
+
       $storeys = $request->only(['storeys']);
+        if(!empty($storeys['storeys'])){
+          foreach ($storeys as $storey => $storeys) {
+            $storeys;
+          }
+        } else $storeys = false;
+
       $garages = $request->only(['garages']);
+        if(!empty($garages['garages'])){
+          foreach ($garages as $garage => $garages) {
+            $garages;
+          }
+        } else $garages = false;
+
+
       return $ser = DB::table('property_data')
-      ->where('name', 'LIKE', '%'.$name.'%')
-      ->orwhereBetween('price', [$priceFrom, $priceTo])
-      ->orwhere('bedrooms', [$bedrooms])
-      ->orwhere('bathrooms', [$bathrooms])
-      ->orwhere('storeys', [$storeys])
-      ->orwhere('garages', [$garages])
-      ->get();
+
+        ->when($name, function($query, $name) {
+          return $query->where('name', 'LIKE', '%'.$name.'%');
+        })
+
+        //->orwhereBetween('price', [$priceFrom, $priceTo])
+
+        ->when($priceFrom, function ($query, $priceFrom) {
+          return $query->where('price', '>=', $priceFrom);
+        })
+
+        ->when($priceTo, function ($query, $priceTo) {
+          return $query->where('price', '<=', $priceTo);
+        })
+
+        ->when($bedrooms, function ($query, $bedrooms) {
+          return $query->where('bedrooms', $bedrooms);
+        })
+
+        ->when($bathrooms, function ($query, $bathrooms) {
+         return $query->where('bathrooms', $bathrooms);
+        })
+
+        ->when($storeys, function ($query, $storeys) {
+          return $query->where('storeys', $storeys);
+        })
+
+        ->when($garages, function ($query, $garages) {
+          return $query->where('garages', $garages);
+        })
+
+        ->get();
     }
 }
